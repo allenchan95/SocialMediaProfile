2277ed81af3b2e36cb8f0c3e0c6dcb13973d6031
allenchan95 <35793136+allenchan95@users.noreply.github.com>
2019-04-13 17:58:29 +0800

    Updates

---
+++ precache-manifest.ee30fc74dcdfaef4af1e508efca9c25a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21336ec0687149fa277f",
+    "url": "/socialmediaProfile/static/js/runtime~main.c217eecc.js"
+  },
+  {
+    "revision": "35f101391ce1c5c2cbfb",
+    "url": "/socialmediaProfile/static/js/main.b0959d25.chunk.js"
+  },
+  {
+    "revision": "b41a33dfeb1b8c4dfe78",
+    "url": "/socialmediaProfile/static/js/2.00d6f609.chunk.js"
+  },
+  {
+    "revision": "35f101391ce1c5c2cbfb",
+    "url": "/socialmediaProfile/static/css/main.29a19005.chunk.css"
+  },
+  {
+    "revision": "9d8a249d7d71dc75a4acb9a6e784785f",
+    "url": "/socialmediaProfile/index.html"
+  }
+];
